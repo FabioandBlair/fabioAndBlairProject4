6487f7433f6c227bacc979578dc05515b4c25433
Blairwin <reach.blair@gmail.com>
2020-02-18 14:57:34 -0500

    Added styles, favicon, and additional api work

--- script.js
+++ script.js
@@ -1,15 +1,73 @@
-$.ajax({
-    url:'https://retroapi.hackeryou.com/api/years/decade',
-    method: 'GET',
-    dataType: 'json',
-    data: {
-        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWU0NzEzMzk5MDk0Y2QwNDNiNzFlNmU3In0sImlhdCI6MTU4MTcxNjI4MSwiZXhwIjoxNjEzMjUyMjgxfQ.FAoVrIYKqTjNsPP8dddwlWL5cOWR8fFJ7qzGlcN0HXw',
-        decade: 1980,
-    }
-}).then(function(result){
-    console.log(result);
-})
+const app = {};
 
+app.getInfo = function (){
+    let chosenYear = $('#year').val();
+    let chosenCategory = $('#category').val();
+
+    $.ajax({
+        url:'https://retroapi.hackeryou.com/api/years/year',
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWU0NzEzMzk5MDk0Y2QwNDNiNzFlNmU3In0sImlhdCI6MTU4MTcxNjI4MSwiZXhwIjoxNjEzMjUyMjgxfQ.FAoVrIYKqTjNsPP8dddwlWL5cOWR8fFJ7qzGlcN0HXw',
+            year: chosenYear
+        }
+    }).then(function(yearResults){
+        console.log(yearResults);
+        console.log(chosenCategory);
+        // ADD an error handling... if year is blank --> please choose a year 
+        // **ADD an error handling... "if chosenCategory = empty string, display all?!"
+        // Figure out how to get the chosenCategory.arrayNumber to print to the page... below prints the category to the page.
+        $('.results p').append(chosenCategory);
+        // $('.results p').append(chosenCategory.forEach([indexOf()])title);... music/array0-4/title 
+
+
+    })  
+
+
+}
+// ***End of app.getInfo Function***
+
+// ***calling the app.getInfo() function when the button is clicked by user***
+$('.goButton').on("click", function() {
+	app.getInfo();
+});
+
+
+// NEED TO FILL THIS IN!!!!!
+// app.init = function(){
+// };
+
+// ***Document ready***
 $(function(){
+    // app.init();
     console.log('working')
 })
+
+
+
+
+
+
+// DELETE BEFORE HANDING IN!!!!!*******************
+// ***************PSEUDO CODE FROM PROJECT GUIDELINES************
+
+
+// // Create app namespace to hold all methods
+// const app = {};
+
+// // Collect user input
+// app.collectInfo = function() {};
+
+// // Make AJAX request with user inputted data
+// app.getInfo = function() {};
+
+// // Display data on the page
+// app.displayInfo = function() {};
+
+// // Start app
+// app.init = function() {};
+
+// $(function() {
+// 	app.init();
+// });
