3f15411e052951f6155ecf10089526e933f32dfb
Blairwin <reach.blair@gmail.com>
2020-02-19 16:42:26 -0500

    changed user input verification to be before the ajax call

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
 const app = {};
 
+app.validateInput = function(chosenYear, chosenCategory) {
+    if (chosenCategory === '' || chosenYear === '') {
+        return false;
+    } 
+    return true
+}
 
 app.getInfo = function (){
     let chosenYear = $("#year").val();
-	let chosenCategory = $("#category").val();
-
-    $.ajax({
+    let chosenCategory = $("#category").val();
+    
+    if (!app.validateInput(chosenYear, chosenCategory) ) {
+            swal('Oops...', 'Please select inputs.', 'warning');
+    } else {
+        $.ajax({
         url:'https://retroapi.hackeryou.com/api/years/year',
         method: 'GET',
         dataType: 'json',
         data: {
             apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWU0NzEzMzk5MDk0Y2QwNDNiNzFlNmU3In0sImlhdCI6MTU4MTcxNjI4MSwiZXhwIjoxNjEzMjUyMjgxfQ.FAoVrIYKqTjNsPP8dddwlWL5cOWR8fFJ7qzGlcN0HXw',
             year: chosenYear
-        }
-    }).then(function(yearResults){
-        $(".resultsFlex").empty();
-        const top5 = yearResults[0][chosenCategory];
-        console.log(yearResults);
-        if (chosenCategory === '') {
-            swal('Oops...', 'Please select a category.', 'warning');
-        }
-        // // Else below not working!!!!!!
-        // else if (chosenYear === '') {
-        //     swal('Please select a year');
-        // }
-        else if(chosenCategory === 'books') {
-            $(".resultsFlex").empty();
-            top5.forEach(function(item) {
-                console.log(item);
-                const htmlToAppend = `
-                    <p><em>${item.title}</em> by ${item.author}</p>
-                `;
-                $('.resultsFlex').append(htmlToAppend);
-            })
-        } else if (chosenCategory === 'songs') {
-            $(".resultsFlex").empty();
-            top5.forEach(function (item) {
-                console.log(item);
-                const htmlToAppend = `
-                    <p><em>${item.title}</em> by ${item.artist}</p>
-                `;
-                $('.resultsFlex').append(htmlToAppend);
-            })
-        }
-            
-            else {
-            $(".resultsFlex").empty();
-            top5.forEach(function(item) {
-            console.log(item);
-            const htmlToAppend = `
-                <p><em>${item.title}</em></p>
-            `;
-                $('.resultsFlex').append(htmlToAppend);
             }
-            );
-        }
-    })};
+        }).then(function(yearResults){
+            $(".resultsFlex").empty();
+            const top5 = yearResults[0][chosenCategory];
+            // console.log(yearResults);
+            // console.log(chosenYear, chosenCategory);
+    
+            if(chosenCategory === 'books') {
+                $(".resultsFlex").empty();
+                top5.forEach(function(item) {
+                    console.log(item);
+                    const htmlToAppend = `
+                        <p><em>${item.title}</em> by ${item.author}</p>
+                    `;
+                    $('.resultsFlex').append(htmlToAppend);
+                })
+            } else if (chosenCategory === 'songs') {
+                $(".resultsFlex").empty();
+                top5.forEach(function (item) {
+                    console.log(item);
+                    const htmlToAppend = `
+                        <p><em>${item.title}</em> by ${item.artist}</p>
+                    `;
+                    $('.resultsFlex').append(htmlToAppend);
+                })
+            }  else {
+                $(".resultsFlex").empty();
+                top5.forEach(function(item) {
+                console.log(item);
+                const htmlToAppend = `
+                    <p><em>${item.title}</em></p>
+                `;
+                    $('.resultsFlex').append(htmlToAppend);
+                }
+                );
+            }
+        })
+    };
+};
+
+        // ajax request returning a Promise
+        // Promise running another function call 
+        
+
+    
         
 
 // ***End of app.getInfo Function***
